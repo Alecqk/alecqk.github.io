fa5be5a1e1b5209d1f8e9e035fef39bdf8da93c5
Alecqk <alrojunkmail@gmail.com>
2026-03-16 20:10:02 +0000

    various fixes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 // Navbar scroll effect
-window.addEventListener('scroll', function() {
-	const navbar = document.querySelector('.navbar');
-	if (window.scrollY > 0) {
-		navbar.classList.add('scrolled');
-	} else {
-		navbar.classList.remove('scrolled');
-	}
-});
+ const navbar = document.querySelector('.navbar');
+ window.addEventListener('scroll', function() {
+ 	if (!navbar) {
+ 		return;
+ 	}
+ 	if (window.scrollY > 0) {
+ 		navbar.classList.add('scrolled');
+ 	} else {
+ 		navbar.classList.remove('scrolled');
+ 	}
+ }, { passive: true });
+
+ window.addEventListener('scroll', handleNavbarScroll);
+ // Initialize navbar state on page load
+ handleNavbarScroll();
 
 // Close burger menu when a link is clicked
 const navbarToggle = document.getElementById('navbar-toggle');
